0efe8c8c8df871630932027881b98d470e5133e3
Tal <taldahan3@gmail.com>
2017-05-17 06:46:10 +0300

    add new files

--- LossCalc.lua
+++ LossCalc.lua
@@ -13,8 +13,8 @@ local cmd = torch.CmdLine()
 -- Dataset options
 cmd:option('-input_h5', 'data/tiny-shakespeare.h5')
 cmd:option('-input_json', 'data/tiny-shakespeare.json')
-cmd:option('-batch_size', 50)
-cmd:option('-seq_length', 50)
+cmd:option('-batch_size', 1)
+cmd:option('-seq_length', 1)
 
 -- Model options
 cmd:option('-init_from', '')
@@ -114,82 +114,22 @@ if opt.memory_benchmark == 1 then
   init_memory_usage = total - free
 end
 
--- Loss function that we pass to an optim method
-local function f(w)
-  assert(w == params)
-  grad_params:zero()
 
-  -- Get a minibatch and run the model forward, maybe timing it
-  local timer
-  local x, y = loader:nextBatch('train')
-  x, y = x:type(dtype), y:type(dtype)
-  if opt.speed_benchmark == 1 then
-    if cutorch then cutorch.synchronize() end
-    timer = torch.Timer()
-  end
-  local scores = model:forward(x)
-
-  -- Use the Criterion to compute loss; we need to reshape the scores to be
-  -- two-dimensional before doing so. Annoying.
-  local scores_view = scores:view(N * T, -1)
-  local y_view = y:view(N * T)
-  local loss = crit:forward(scores_view, y_view)
-
-  -- Run the Criterion and model backward to compute gradients, maybe timing it
-  local grad_scores = crit:backward(scores_view, y_view):view(N, T, -1)
-  model:backward(x, grad_scores)
-  if timer then
-    if cutorch then cutorch.synchronize() end
-    local time = timer:time().real
-    print('Forward / Backward pass took ', time)
-    table.insert(forward_backward_times, time)
-  end
-
-  -- Maybe record memory usage
-  if opt.memory_benchmark == 1 then
-    assert(cutorch)
-    if cutorch then cutorch.synchronize() end
-    local free, total = cutorch.getMemoryUsage(cutorch.getDevice())
-    local memory_used = total - free - init_memory_usage
-    local memory_used_mb = memory_used / 1024 / 1024
-    print(string.format('Using %dMB of memory', memory_used_mb))
-    table.insert(memory_usage, memory_used)
-  end
-
-  if opt.grad_clip > 0 then
-    grad_params:clamp(-opt.grad_clip, opt.grad_clip)
-  end
-
-  return loss, grad_params
+-- Evaluate loss on the validation set. Note that we reset the state of
+-- the model; this might happen in the middle of an epoch, but that
+-- shouldn't cause too much trouble.
+model:evaluate()
+model:resetStates()
+local num_val = loader.split_sizes['val']
+local val_loss = 0
+for j = 1, num_val do
+	local xv, yv = loader:nextBatch('val')
+	xv = xv:type(dtype)
+	yv = yv:type(dtype):view(N * T)
+	local scores = model:forward(xv):view(N * T, -1)
+	val_loss = val_loss + crit:forward(scores, yv)	
 end
+val_loss = val_loss / num_val
+print('val_loss = ', val_loss)
+  
 
--- Train the model!
-local optim_config = {learningRate = opt.learning_rate}
-local num_train = loader.split_sizes['train']
-local num_iterations = opt.max_epochs * num_train
-model:training()
-for i = start_i + 1, num_iterations do
-  local epoch = math.floor(i / num_train) + 1
-
-  -- Check if we are at the end of an epoch
-  if i % num_train == 0 then
-    model:resetStates() -- Reset hidden states
-
-    -- Maybe decay learning rate
-    if epoch % opt.lr_decay_every == 0 then
-      local old_lr = optim_config.learningRate
-      optim_config = {learningRate = old_lr * opt.lr_decay_factor}
-    end
-  end
-
-  -- Take a gradient step and maybe print
-  -- Note that adam returns a singleton array of losses
-  local _, loss = optim.adam(f, params, optim_config)
-  table.insert(train_loss_history, loss[1])
-  if opt.print_every > 0 and i % opt.print_every == 0 then
-    local float_epoch = i / num_train + 1
-    local msg = 'Epoch %.2f / %d, i = %d / %d, loss = %f'
-    local args = {msg, float_epoch, opt.max_epochs, i, num_iterations, loss[1]}
-    print(string.format(unpack(args)))
-  end
-end
--- rnnWrapper.py
+++ rnnWrapper.py
@@ -64,50 +64,90 @@ def sample(param):
     #print (proc)
     return 0 , proc
     
-    
 def sampleFile(param,numOfIterations):        
-    status, res = sample(param)
     _param = copy.copy(param)
+    #add length variable to list
+    _param.append('-length')
+    _param.append('0')
     lengthIdx = _param.index('-length')
-    lengthIdx = lengthIdx + 1
-    length = int(_param[lengthIdx])
+    lengthIdx = lengthIdx + 1       
     fileIdx = _param.index('-start_file')
     fileIdx = fileIdx+1
     
-    #fileType = _param[fileIdx] [:(len(_param[fileIdx])-4)]    
-    #startFile = _param[fileIdx][:-4]       
-    #newFile = startFile+'0'+fileType
-    startFile = _param[fileIdx]      
-    newFile = startFile+'0'
-	
-    
-    with open(newFile,'w+') as tempFile:
-        tempFile.write(res)     
-    #copy_rename(startFile,newFile)
-    #changing params from start_file to start_text    
-    #_param = changeParamValue(param,"sample","start_file","")
-    #_param = changeParamValue(_param,"sample","start_file_val","")
-    #_param = changeParamValue(_param,"sample","start_text","-start_text")
-    #_param = changeParamValue(_param,"sample","start_text_val",res)
+    #getting file length and updating the param list   
+    with open(_param[fileIdx],"r") as f:
+        _param[lengthIdx] = len(f.read())
+        
+    length = int(_param[lengthIdx])
     
     _param[lengthIdx] = str(length + length)
-    for i in range(1,numOfIterations):                
+        
+    startFile = _param[fileIdx]      
+    newFile = startFile  
+    
+    paramForPreProc =["python","scripts/preprocess.py","--input_txt",\
+    "","--output_h5",".h5","--output_json",".json","--val_frac","0.3","--test_frac","0.3"]
+    
+    paramForLossCalc = ["th","LossCalc.lua","-init_from","cv/checkpoint_41520.t7","-input_h5",\
+    ".h5","-input_json",".json","-gpu","-1",]
+    
+    input_idx = 3
+    outputH5_idx = 5
+    outputJSON_idx = 7
+    
+    #creating file for the resaults - override if exists
+    with open("Results.txt","w") as resFile:
+        resFile.write("Start Sampling")
+        
+        
+    
+    for i in range(0,numOfIterations):                
         
         _param[fileIdx] = newFile
         status, res = sample(_param)
         res = res[length:]
         if status != 0:
             return 1
-        #os.remove(newFile)     
+        
+        #creating a new file in each iteration
         newFile = startFile+str(i)
         with open(newFile,'w+') as tempFile:
             tempFile.write(res)
-    
+        
+        #Pre process in order to calculate total loss
+        paramForPreProc[input_idx] = newFile
+        paramForPreProc[outputH5_idx] = newFile+paramForPreProc[outputH5_idx]
+        paramForPreProc[outputJSON_idx] = newFile+paramForPreProc[outputJSON_idx]
+        if(preProcess(paramForPreProc) <> 0):
+            return 1
+        paramForPreProc = ["python","scripts/preprocess.py","--input_txt",\
+        "","--output_h5",".h5","--output_json",".json","--val_frac","0.3","--test_frac","0.3"]
+        
+        #calculate total loss
+        
+        paramForLossCalc[outputH5_idx] = newFile+paramForLossCalc[outputH5_idx]
+        paramForLossCalc[outputJSON_idx] = newFile+paramForLossCalc[outputJSON_idx]
+        proc = subprocess.check_output(paramForLossCalc)
+        if ( proc == ""):
+            return 1
+        
+        #parsing results
+        resList = proc.split("val_loss =")
+        val_loss = resList[1]
+        val_loss = '\n'+val_loss.strip()
+        
+        #print resaults to file
+        with open("Results.txt","a") as resFile:
+            resFile.write(val_loss)
+            
+        paramForLossCalc = ["th","LossCalc.lua","-init_from","cv/checkpoint_41520.t7","-input_h5",\
+        ".h5","-input_json",".json","-gpu","-1",]
+              
         
     os.rename(newFile,startFile+"_Final_OUTPUT") 
     #os.remove(newFile)               
-    return 0         
-    
+    return 0   
+
          
 def train (param):
     """
@@ -156,8 +196,12 @@ def start():
 
 if __name__ == "__main__":
     sampleList = ["th","sample.lua","-checkpoint",\
-    "cv/checkpoint_10000.t7","-length","2000","-start_file","input text/Tal_King","-gpu","-1"]
-    sampleFile(sampleList,10)
+    "cv/checkpoint_10000.t7","-start_file","input text/Tal_King","-gpu","-1"]
+    sampleFile(sampleList,10000)
+
+    #trainList = ["th","train.lua","-input_h5",\
+    #"data/big.h5","-input_json","data/big.json","-gpu","-1","-max_epochs","20"]
+    #train(trainList)
 
 
 
